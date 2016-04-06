309db24c37c77abd61e3803e602f8576f32303c6
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-04-06 14:23:47 -0300

    Add devise and create user model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_MSG = {
+    success: 'alert-success',
+    error: 'alert-error',
+    alert: 'alert-alert',
+    notice: 'alert-notice',
+    info: 'alert-info'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type, flash_type.to_s)
+  end
+
+  def bootstrap_icon_for flash_type
+    { success: "ok-circle", error: "remove-circle", alert: "warning-sign", notice: "exclamation-sign" }[flash_type] || "question-sign"
+  end
 end
