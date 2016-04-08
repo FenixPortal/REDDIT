c86054614e9e4c7aefa687a5defac4e1d77cb940
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-04-08 13:51:48 -0300

    Add comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.new(comment_params)
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @link, notice: 'Comment was successfully updated.' }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { render action: :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:link_id, :body, :user_id)
+    end
+end
